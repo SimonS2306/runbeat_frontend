c763af3d42b8ca01903a2efb09a46c875c10fd7b
Christina Kopp <Christina Kopp>
2016-06-09 15:17:58 +0200

    Fixed minor ui errors (login), automatic login redirect, halfdynamic header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,21 @@
-angular.module('RunBeatApp').run(function ($rootScope, AuthService) {
+angular.module('RunBeatApp').run(function ($rootScope, $location, AuthService) {
     $rootScope.$on('$stateChangeStart',
         function (event, toState) {
             var requireLogin = toState.access && toState.access.restricted;
 
             if (requireLogin && AuthService.isLoggedIn() === false) {
-                event.preventDefault();
+                $location.path('login');
             }
         });
 });
+
+angular.module('RunBeatApp').controller('headerController', ['$scope', 'AuthService',
+    function ($scope, AuthService) {
+
+        $scope.AuthService = AuthService;
+
+        $scope.$watch('AuthService.isLoggedIn()', function(newVal) {
+            $scope.isLoggedIn = newVal;
+        });
+
+}]);
