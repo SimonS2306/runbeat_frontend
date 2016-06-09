cb7a47ce2fe94acdd219b2fd76c6f13737f842c8
Christina Kopp <Christina Kopp>
2016-06-09 14:38:08 +0200

    Fixed run bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
-angular.module('RunBeatApp').run(function ($rootScope, $location, $route, AuthService) {
-    $rootScope.$on('$routeChangeStart',
-        function (event, next, current) {
-            if (AuthService.isLoggedIn() === false) {
-                $location.path('/');
+angular.module('RunBeatApp').run(function ($rootScope, AuthService) {
+    $rootScope.$on('$stateChangeStart',
+        function (event, toState) {
+            var requireLogin = toState.access && toState.access.restricted;
+
+            if (requireLogin && AuthService.isLoggedIn() === false) {
+                event.preventDefault();
             }
         });
 });
