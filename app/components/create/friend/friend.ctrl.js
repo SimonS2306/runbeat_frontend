9b9723aed722dc54a931c69512c7717d3b81299c
sspitzer <simon-spitzer@web.de>
2016-07-03 04:24:47 +0200

    update profile page + communication with backend still errors when trying to update profile

--- app/components/create/friend/friend.ctrl.js
+++ app/components/create/friend/friend.ctrl.js
@@ -7,7 +7,7 @@
     friendController.$inject = ["$scope", "sharedCreateChallenge"];
 
     function friendController($scope, sharedCreateChallenge) {
-
+        console.log(user)
         $scope.update = function(user) {
             sharedCreateChallenge.setName(user);
         };
--- app/components/profile/profile.ctrl.js
+++ app/components/profile/profile.ctrl.js
@@ -1,70 +1,20 @@
-angular.module('RunBeatApp').controller('profileController',
-    ['$scope', '$location','$http', 'ProfileService',
-        function ($scope, $location,$http, ProfileService) {
-            
-            $scope.user = { username: '123456',
-                            profilePicture: 'assets/img/Profile.jpg',
-                            credo: 'Carpe Diem',
-                            email: 'hello@hello.com',
+angular.module('RunBeatApp')
+    .controller('profileController', function ($scope, $location, $http) {
+
+            $scope.user = { name: '',
+                            profileImagePath: '',
+                            credo: '',
+                            firstname: '',
+                            lastname: '',
                             birthday: ''
             };
-            $scope.updateProfile=function() {
-                console.log($scope.user);
-                var myRes=$http.post('http://localhost:3000/user/update',$scope.user);
+        
+            $scope.updateProfile= updateProfile;
+                
+                function updateProfile() {
+                var myRes=$http.post('http://localhost:3000/profile/simon' /* +currUser.getUsername() */, $scope.user);
                 console.log(myRes);
+                $location.path('/profile');
             };
-            /*$scope.user = ProfileService.getProfile();*/
-
-        }
-    ]);
-
-angular.module('RunBeatApp').factory('ProfileService',
-    ['$q', '$timeout', '$http',
-        function ($q, $timeout, $http) {
-
-            var user = [];
-
-            // return available functions for use in the controllers
-            return ({
-                getProfile: getProfile,
-                setProfile: setProfile
-            });
-
-
-            function getProfile() {
-                // create a new instance of deferred
-                var deferred = $q.defer();
-
-                // send a post request to the server
-                /*$http.get('http://localhost:3000/user/getProfile',
-                    {userid: userid})
-                // handle success
-                    .success(function (data, status) {
-                        if (status === 200 && data.status) {
-                            user = true;
-                            deferred.resolve();
-                        } else {
-                            user = false;
-                            deferred.reject();
-                        }
-                    })
-                    // handle error
-                    .error(function (data) {
-                        user = false;
-                        deferred.reject();
-                    }); TODO*/
-
-                // return promise object
-                return deferred.promise;
-            }
-
-            function setProfile() {
-                // create a new instance of deferred
-                var deferred = $q.defer();
-                /*TODO*/
-                return deferred.promise;
-            }
-
-
-        }
-    ]);
+            
+});
